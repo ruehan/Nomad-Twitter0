c19aab9d8aa74b8b6948e488712f68d1c919aa87
vendredl <node.js_@kakao.com>
2023-11-06 12:30:27 +0900

    :lipstick: 로그인 페이지 임시 구현

---
+++ pages/log-in.tsx
@@ -0,0 +1,71 @@
+import type { NextPage } from 'next';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
+
+type FormData = {
+    nickname: string;
+    email: string;
+    password: string;
+}
+
+const Login: NextPage = () => {
+    const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
+
+    const router = useRouter();
+
+    const onSubmit = async (data: FormData) => {
+        const response = await fetch('/api/log-in', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+
+        const result = await response.json()
+        console.log(result)
+
+        router.push("/")
+    }
+
+  return (
+    <>
+      <section className='flex justify-center'>
+        <div className='border-2 border-black h-screen max-w-md w-full relative'>
+            <header className='w-full h-1/2 flex justify-center items-center'>
+                <div className='font-dancing text-6xl'>Hangram</div>
+            </header>
+            <main className='w-full h-1/4 '>
+                <form onSubmit={handleSubmit(onSubmit)} className='w-full h-full flex flex-col justify-center items-center'>
+                {errors.email && <span className='text-red-500 font-bold text-sm'>{errors.email.message}</span>}
+                <input
+                    {...register('email', {
+                        required: '이메일을 입력해주세요.',
+                        pattern: {
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                        message: '올바르지 않은 이메일 형식 입니다.'
+                        }
+                    })}
+                    className="mt-4 border border-gray-200 h-10 w-5/6 rounded-xl" />
+
+                {errors.password && <span className='mt-4 text-red-500 font-bold text-sm'>비밀번호를 입력해주세요.</span>}
+                <input
+                    type="password"
+                    {...register('password', { required: true })}
+                    className="mt-4 border border-gray-200 h-10 w-5/6 rounded-xl" />
+
+                <button type="submit" className="mt-4 rounded-3xl w-5/6 h-10 rounded-2xl text-white bg-blue-600">로그인</button>
+                <Link href={'/'} className='text-sm mt-2 '>비밀번호를 잊으셨나요?</Link>
+                </form>
+            </main>
+            <footer className='w-full h-1/5 flex justify-center items-end'>
+                <button type="submit" className="mt-4 rounded-3xl w-5/6 h-10 rounded-2xl text-blue-600 border border-blue-600">새 계정 만들기</button>
+            </footer>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Login;
