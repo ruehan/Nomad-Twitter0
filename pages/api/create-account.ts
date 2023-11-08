accfec932f54f54897a2760d1a227155012dc136
vendredl <node.js_@kakao.com>
2023-11-08 16:59:52 +0900

    :alien: Create-Account API 추가

---
+++ pages/api/create-account.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handle(req: NextApiRequest, res: NextApiResponse){
+    if (req.method === 'POST'){
+        const {nickname, email, password} = req.body;
+
+        try{
+            const user = await prisma.user.create({
+                data: {
+                    nickname: nickname,
+                    email: email,
+                    password: password
+                }
+            })
+
+            return res.status(200).json(user)
+        } catch(error){
+            return res.status(400).json({message: 'Something went wrong.'})
+        }
+    } else {
+        return res.status(405).end()
+    }
+}
