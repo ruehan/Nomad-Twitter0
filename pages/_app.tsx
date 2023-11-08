f0b428cfa74238906c3cda6ffdcc3e321a7c6bbb
vendredl <node.js_@kakao.com>
2023-11-08 17:19:44 +0900

    :lipstick: AnimatePresence 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import { SWRConfig } from "swr";
 import "../global.css";
+import { AnimatePresence } from "framer-motion";
 
 export default function App({ Component, pageProps }: any) {
   return (
-    <SWRConfig
-      value={{
-        fetcher: (url: string) => fetch(url).then((response) => response.json())
-      }}
-    >
-      <Component {...pageProps} />
-    </SWRConfig>
+    <AnimatePresence>
+        <SWRConfig
+        value={{
+          fetcher: (url: string) => fetch(url).then((response) => response.json())
+        }}
+      >
+        <Component {...pageProps} />
+      </SWRConfig>
+    </AnimatePresence>
   );
 }
