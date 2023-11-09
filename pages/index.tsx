8e1a37a325b71355cae366ff42d927265c63bd74
vendredl <node.js_@kakao.com>
2023-11-09 17:26:59 +0900

    :lipstick: 메인화면 tweet DB와 연동

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,15 @@ import {MdOutlineAddCircleOutline as AddIcon} from 'react-icons/md'
 import {CgProfile as ProfileIcon} from 'react-icons/cg'
 import useSWR from 'swr';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 interface LoginInfo {
   loggedIn: boolean;
   user?: any;
+}
+interface TweetInfo {
+  tweets: any;
 }
 
 const fetcher = async (url: string): Promise<LoginInfo> => {
@@ -20,9 +25,45 @@ const fetcher = async (url: string): Promise<LoginInfo> => {
   return res.json();
 };
 
+const tweet_fetcher = async (url: string): Promise<TweetInfo> => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error('An error occurred while fetching the data.');
+  }
+  return res.json();
+};
+
 const Home: NextPage = () => {
 
   const {data, error} = useSWR<LoginInfo>('/api/user-data', fetcher)
+  const { data :tweets, error : tweetsError } = useSWR<TweetInfo>('/api/get-tweet', tweet_fetcher);
+  const [image, setImage] = useState<string[]>([])
+
+// useEffect(() => {
+//   if (tweets) {
+//     const newImages = tweets.tweets.reduce((acc: any, tweet: any) => {
+//       if (tweet.images !== undefined) {
+//         return [...acc, ...tweet.images.split(',')];
+//       }
+//       return acc;
+//     }, []);
+
+//     setImage((prevImages) => [...prevImages, ...newImages]);
+//   }
+// }, [tweets]);
+
+useEffect(() => {
+  if (tweets) {
+    const newImages = tweets.tweets.flatMap((tweet: any) => 
+      tweet.images ? tweet.images.split(',') : []
+    );
+
+    // Set을 사용하여 중복을 제거하고 기존 이미지들과 새로운 이미지들을 합칩니다.
+    setImage((prevImages) => [...new Set([...prevImages, ...newImages])]);
+  }
+}, [tweets]);
+
+  console.log(image)
 
   const router = useRouter();
 
@@ -36,6 +77,9 @@ const Home: NextPage = () => {
   `
 
   if (!data) return <div>Loading...</div>;
+  if (!tweets) return <div>Loading...</div>
+
+  console.log(tweets)
 
   if(data.loggedIn !== true){
     router.push('/log-in')
@@ -44,14 +88,16 @@ const Home: NextPage = () => {
   return (
     <>
       <section className='flex justify-center'>
-      <div className='border border-black h-screen max-w-md relative'>
-        <header className='flex justify-between items-center w-full h-12 text-2xl font-bold p-4'>
+      <div className='border border-gray-100 h-screen max-w-md relative overflow-scroll scrollbar-hide'>
+        <header className='sticky bg-white top-0 left-0 flex justify-between items-center w-full h-12 text-2xl font-bold p-4'>
           <div className='font-dancing'>Hangram</div>
           <div> 
             <HeartIcon />
           </div>
         </header>
-        <div className='w-full h-24 border-b-2 border-gray-200 flex items-center pl-4 overflow-scroll scrollbar-hide'>
+        
+        <div className='sticky top-12 left-0 bg-white w-full h-24 border-b-2 border-gray-200 flex items-center pl-4 overflow-scroll scrollbar-hide'>
+          
           <div className="w-16 h-full flex flex-col justify-center items-center m-2">
             <div className="border-2 border-gray-300 w-16 h-16 rounded-full"></div>
             <div className="text-xs">ruehan_98</div>
@@ -80,14 +126,26 @@ const Home: NextPage = () => {
 
         <main>
           <div className='card'>
-            <div className="header h-12 border-b-2 border-gray-200 flex items-center p-4">
-              <div className="img h-8 w-8 rounded-full border-2 border-black"></div>
-              <div className="nickname text-sm ml-4 font-bold">nomad_coders</div>
-            </div>
-            <div className="main w-full max-h-max bg-blue-300">
-              {word}
-            </div>
-            <div className="footer h-8 border-b-2 border-gray-200 text-xl flex p-2 items-center">
+            {tweets.tweets.map((tweet: any) => (
+              <div className='w-full overflow-hidden'>
+              <div className="header h-12 border-b-2 border-gray-200 flex items-center p-4">
+                <div className="img h-8 w-8 rounded-full border-2 border-black"></div>
+                <div className="nickname text-sm ml-4 font-bold">{tweet.nickname}</div>
+              </div>
+              <div className='flex w-full overflow-scroll scrollbar-hide'>
+                {image.map((img: string) => (
+                   tweet.images.includes(img) ? (
+                    <>
+                    <img key={tweet.id} src={img.replace('public', '')} width="90%" className="rounded-xl m-4" />
+                    </>
+                   ) : null
+                )
+                )}
+              </div>
+              <div className="main w-full max-h-max pl-4">
+                {tweet.content}
+              </div>
+              <div className="footer h-8 border-b-2 border-gray-200 text-xl flex p-2 items-center">
               <div className='m-2'>
                 <HeartIcon />
               </div>
@@ -95,11 +153,15 @@ const Home: NextPage = () => {
                 <MessageIcon />
               </div>
             </div>
+              </div>
+            ))}
+            
+            
           </div>
         </main>
-        <footer className='flex justify-between items-center w-full h-12 text-2xl font-bold p-4 absolute left-0 bottom-0 border-t border-gray-200'>
+        <footer className='sticky bg-white bottom-0 left-0 flex justify-between items-center w-full h-12 text-2xl font-bold p-4 absolute left-0 bottom-0 border-t border-gray-200 z-10'>
           <HomeIcon />
-          <AddIcon />
+          <Link href='/create-tweet'><AddIcon /></Link>
           <ProfileIcon />
         </footer>
       </div>
